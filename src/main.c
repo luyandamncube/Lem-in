cd97012cb5372d1a8e2431197767fda0ce9a3aac
luyandamncube <59448873@mylife.unisa.ac.za>
2018-09-14 00:16:46 +0200

    started DFS

--- src/main.c
+++ src/main.c
@@ -13,19 +13,47 @@
 #include "lem_in.h"
 #include <stdio.h> //REMOVE ME
 
+//assumes start is at least first element in graph
+void		DFS(t_graph *graph, int start, int *visited)
+{
+	int	k;
+
+	k = start;
+	visited[start] = 1;
+	printf("%d \n", start);
+
+	while(graph->array[k].head)
+	{
+		printf("count %d\n", k);
+		if (!visited[k])
+		{
+			DFS(graph, k, visited);
+		}
+		k++;
+	}	
+}
+
 int		main(void)
 {
 	t_graph		*graph;
+	int		visited[10] = {0};
 	
 	graph = create_graph(10);
-	edge(graph, "789");
-	edge(graph, "quirky");
-	edge(graph, "nasty");
-	add_link(graph, "789", "quirky");
+	edge(graph, "0");
+	edge(graph, "1");
+	edge(graph, "2");
+	edge(graph, "3");
+	add_link(graph, "0", "1");
+	add_link(graph, "0", "2");
+	add_link(graph, "1", "2");
+	add_link(graph, "2", "0");
+	add_link(graph, "2", "3");
+	add_link(graph, "3", "3");
 	printf("======= adj list at index 0 ====== \n");
 	dump_list(graph->array[0]);
 	printf("======= graph  ====== \n");
 	dump_graph(graph);
+	DFS(graph,1,visited);
 	free_all(graph);
 	/*
 	   n1 = create_node(0, "789");
