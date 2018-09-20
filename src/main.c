dbee9f1ef8e367a142d06bd48e94edd9f827cd26
Luyanda MNCUBE <lmncube@e4r21p1.wethinkcode.co.za>
2018-09-20 16:47:55 +0200

    zzzzzz

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lmncube <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/12 12:12:19 by lmncube           #+#    #+#             */
-/*   Updated: 2018/09/20 14:56:47 by lmncube          ###   ########.fr       */
+/*   Updated: 2018/09/20 16:46:33 by lmncube          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,24 +37,39 @@ int		main(void)
 	char		**map;
 	int			k;
 	int			k_count;
+	int			m_count;
 	int			m;
 
 	stack = create_stack();
 	graph = create_graph(8);
 	map = read_map();
 	start(graph, stack, map);
-	k = -1;
-	while (++k < graph->path_size)
+	k = 0;
+	while (k < graph->ants)
 	{
 		m = k;
 		k_count = k;
-		while (m-- > 0)
-		{
-			
-			printf("L%d = %s ", m+1, find_name(graph, graph->path[k_count-- - 1]));
+		m_count = 0;
+		if (k < graph->path_size + 1)
+		{	
+			while (m-- > 0)
+			{
+				m_count++;
+				printf("L%d = %s ", m_count, find_name(graph, graph->path[--k_count]));
+			}
+			printf("\n");
 		}
-		printf("\n");
+		else
+		{
+			printf("other\n");
+		/*while (m++ < graph->ants)
+		{
+				printf("L%d = %s ", m+1, find_name(graph, graph->path[k_count-- - 1]));
+		}*/
+		}
+		k++;
 	}
+	printf("\n");
 	free_all(graph, stack, map);
 	return (0);
 }
