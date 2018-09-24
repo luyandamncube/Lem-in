c7a66f2c1677fb3e311060dfe8218801723613b5
Luyanda MNCUBE <lmncube@e4r21p1.wethinkcode.co.za>
2018-09-24 15:09:01 +0200

    fixed weird map output

--- src/output.c
+++ src/output.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   output.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmncube <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/24 14:09:22 by lmncube           #+#    #+#             */
+/*   Updated: 2018/09/24 15:06:50 by lmncube          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "lem_in.h"
 
 void    part_1(t_graph *graph, t_stack *path, t_stack *ants, int LO)
@@ -27,17 +39,18 @@ void    part_1(t_graph *graph, t_stack *path, t_stack *ants, int LO)
 
 void    part_2(t_graph *graph, t_stack *path, t_stack *ants, int LO, int ABS)
 {
-	int     q[4];
-
+	int     q[5];
+	
 	q[0] = 0;
 	q[3] = ants->top;
-	printf ("ABS %d\n", ABS);
+	graph->path_size == LO ? q[4] = 0: (q[4] = 1);
 	while (q[0] < ABS)
 	{
 		printf("PART 2 \n");
-		q[1] = path->top;
-		graph->path_size == LO ? (q[1]) : (q[1] -= 1); //path index
-		graph->path_size == LO ? q[2] = q[0] + 1: q[0]; //ants index
+
+		q[1] = path->top - 1; //path index
+		graph->path_size == LO ? q[2] = q[0] + 1: (q[2] = 1); //ants index
+		printf("q[2] value %d\n", q[2]);
 		ft_push(q[3]++, ants);
 		while (q[1]-- > 0)
 		{
@@ -45,9 +58,9 @@ void    part_2(t_graph *graph, t_stack *path, t_stack *ants, int LO, int ABS)
 			if (graph->path_size == LO)
 				ft_putnbr(ants->s[++q[2]]);
 			else
-				ft_putnbr(ants->s[q[2]]); //UNTESTED. TEST with 4 ants, 8 moves
+				ft_putnbr(ants->s[q[2]++]); //UNTESTED. TEST with 4 ants, 8 moves
 			ft_putstr(" = ");
-			ft_putstr(find_name(graph, path->s[q[1]]));
+			ft_putstr(find_name(graph, path->s[q[1] + q[4]]));
 			ft_putstr(" ");
 		}
 		ft_putstr("\n");
@@ -58,20 +71,23 @@ void    part_2(t_graph *graph, t_stack *path, t_stack *ants, int LO, int ABS)
 void    part_3(t_graph *graph, t_stack *path, t_stack *ants, int LO)
 {
 	int     q[5];
+	int		weirdmap;
 
 	q[0] = 0;
 	q[4] = LO - 1;
 	q[3] = ants->s[ants->top - q[4]];
+	graph->path_size > LO ? (weirdmap = 1) : (weirdmap = 0);
 	while (++q[0] < LO)
 	{
 		q[1] = q[4]--; //path index
 		q[2] = ants->s[q[3]]; //ants index
+		graph->path_size > LO ? q[2]-- : q[2];
 		while (q[1]-- > 0)
 		{
 			ft_putstr("L");
 			ft_putnbr(ants->s[q[2]++]);
 			ft_putstr(" = ");
-			ft_putstr(find_name(graph, path->s[q[1] + q[0]]));
+			ft_putstr(find_name(graph, path->s[q[1] + q[0] + weirdmap]));
 			ft_putstr(" ");
 		}
 		ft_putstr("\n");
